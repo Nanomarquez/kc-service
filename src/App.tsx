cc6c713489784add475201f0c8602cb7ffe7ff77
Federico Marquez <federico.marquez@redtecnologica.org>
2026-01-02 13:52:40 -0300

    indexs

--- src/App.tsx
+++ src/App.tsx
@@ -63,32 +63,88 @@ function App() {
               "streetAddress": "H. Leguizamón 693",
               "addressLocality": "Pilar",
               "addressRegion": "Buenos Aires",
-              "addressCountry": "AR"
+              "addressCountry": "AR",
+              "postalCode": "B1630"
             },
             "geo": {
               "@type": "GeoCoordinates",
               "latitude": -34.4589,
               "longitude": -58.9142
             },
-            "openingHoursSpecification": {
-              "@type": "OpeningHoursSpecification",
-              "dayOfWeek": [
-                "Monday",
-                "Tuesday",
-                "Wednesday",
-                "Thursday",
-                "Friday",
-                "Saturday"
-              ],
-              "opens": "09:00",
-              "closes": "20:00"
-            },
+            "openingHoursSpecification": [
+              {
+                "@type": "OpeningHoursSpecification",
+                "dayOfWeek": [
+                  "Monday",
+                  "Tuesday",
+                  "Wednesday",
+                  "Thursday",
+                  "Friday",
+                  "Saturday"
+                ],
+                "opens": "09:00",
+                "closes": "20:00"
+              }
+            ],
             "serviceArea": {
               "@type": "City",
               "name": "Pilar"
             },
             "priceRange": "$$",
-            "image": "https://www.kc-service.com.ar/og-image.png"
+            "image": "https://www.kc-service.com.ar/og-image.png",
+            "logo": "https://www.kc-service.com.ar/logo-kc-service.png",
+            "potentialAction": [
+              {
+                "@type": "CommunicateAction",
+                "target": {
+                  "@type": "EntryPoint",
+                  "urlTemplate": "https://wa.me/541141611724",
+                  "actionPlatform": [
+                    "http://schema.org/WhatsAppAction",
+                    "https://www.google.com/search?q=kc+service"
+                  ]
+                },
+                "result": {
+                  "@type": "Message",
+                  "name": "Enviar WhatsApp a KC Service"
+                }
+              },
+              {
+                "@type": "CommunicateAction",
+                "target": {
+                  "@type": "EntryPoint",
+                  "urlTemplate": "tel:+541141611724",
+                  "actionPlatform": [
+                    "http://schema.org/TelephoneAction",
+                    "https://www.google.com/search?q=kc+service"
+                  ]
+                },
+                "result": {
+                  "@type": "CallAction",
+                  "name": "Llamar a KC Service"
+                }
+              },
+              {
+                "@type": "ViewAction",
+                "target": {
+                  "@type": "EntryPoint",
+                  "urlTemplate": "https://www.google.com/maps/place/BUM,+Dr.+H.+Leguizam%C3%B3n+693,+B1630+Pilar+Centro,+Provincia+de+Buenos+Aires",
+                  "actionPlatform": [
+                    "http://schema.org/WebPlatform",
+                    "https://www.google.com/search?q=kc+service"
+                  ]
+                },
+                "result": {
+                  "@type": "Place",
+                  "name": "Ver ubicación de KC Service"
+                }
+              }
+            ],
+            "areaServed": {
+              "@type": "City",
+              "name": "Pilar",
+              "sameAs": "https://es.wikipedia.org/wiki/Pilar_(Buenos_Aires)"
+            }
           })}
         </script>
         
@@ -100,14 +156,79 @@ function App() {
             "name": "KC Service",
             "url": "https://www.kc-service.com.ar",
             "logo": "https://www.kc-service.com.ar/logo-kc-service.png",
-            "contactPoint": {
-              "@type": "ContactPoint",
-              "telephone": "+541141611724",
-              "contactType": "customer service",
-              "availableLanguage": "Spanish"
-            },
+            "contactPoint": [
+              {
+                "@type": "ContactPoint",
+                "telephone": "+541141611724",
+                "contactType": "customer service",
+                "availableLanguage": ["Spanish", "es"],
+                "areaServed": "AR",
+                "contactOption": ["TollFree", "HearingImpairedSupported"]
+              }
+            ],
             "sameAs": [
               "https://wa.me/541141611724"
+            ]
+          })}
+        </script>
+        
+        {/* Structured Data - WebSite */}
+        <script type="application/ld+json">
+          {JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "WebSite",
+            "name": "KC Service - Reparación de Electrodomésticos",
+            "url": "https://www.kc-service.com.ar",
+            "description": "Servicio profesional de reparación de electrodomésticos en Pilar, Buenos Aires",
+            "publisher": {
+              "@type": "Organization",
+              "name": "KC Service",
+              "logo": {
+                "@type": "ImageObject",
+                "url": "https://www.kc-service.com.ar/logo-kc-service.png"
+              }
+            },
+            "potentialAction": {
+              "@type": "SearchAction",
+              "target": {
+                "@type": "EntryPoint",
+                "urlTemplate": "https://www.kc-service.com.ar/?q={search_term_string}"
+              },
+              "query-input": "required name=search_term_string"
+            }
+          })}
+        </script>
+        
+        {/* Structured Data - BreadcrumbList */}
+        <script type="application/ld+json">
+          {JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "BreadcrumbList",
+            "itemListElement": [
+              {
+                "@type": "ListItem",
+                "position": 1,
+                "name": "Inicio",
+                "item": "https://www.kc-service.com.ar/#inicio"
+              },
+              {
+                "@type": "ListItem",
+                "position": 2,
+                "name": "Servicios",
+                "item": "https://www.kc-service.com.ar/#servicios"
+              },
+              {
+                "@type": "ListItem",
+                "position": 3,
+                "name": "Nosotros",
+                "item": "https://www.kc-service.com.ar/#nosotros"
+              },
+              {
+                "@type": "ListItem",
+                "position": 4,
+                "name": "Contacto",
+                "item": "https://www.kc-service.com.ar/#contacto"
+              }
             ]
           })}
         </script>
